c9a95e43d7bbe567c03bbb9bae5bb26df09373f6
Alejandro Ramirez <emiramirez991@gmail.com>
2021-08-26 14:25:46 -0500

    make it better

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -73,7 +73,7 @@ exports.actualizarTarea = async (req, res) => {
         const {proyecto, nombre, estado} = req.body;
 
         // Si la tarea existe o no
-        let tarea = await Tarea.findById(req.params._id);
+        let tarea = await Tarea.findById(req.params.id);
         if(!tarea) {
             return res.status(404).json({msg: 'No existe esa tarea'});
         }
@@ -82,7 +82,7 @@ exports.actualizarTarea = async (req, res) => {
         const existeProyecto = await Proyecto.findById(proyecto);
 
         // Revisar si el proyecto actual pertencese al usuairo autenticado
-        if(existeProyecto.creador.toString() !== req.usuario._id) {
+        if(existeProyecto.creador.toString() !== req.usuario.id) {
             return res.status(401).json({msg: 'No Autorizado'});
         }
 
